9f92dfc5f98f2618070242cec44236ba6118729a
MatheusVict <matheusvictorhenrique@gmail.com>
2024-06-23 20:05:36 -0300

    feat: add category service methods implementation
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

--- src/main/java/br/com/buzzmonitor/book_e_commerce/service/CategoryService.java
+++ src/main/java/br/com/buzzmonitor/book_e_commerce/service/CategoryService.java
@@ -1,5 +1,6 @@
 package br.com.buzzmonitor.book_e_commerce.service;
 
+import br.com.buzzmonitor.book_e_commerce.domain.Category;
 import br.com.buzzmonitor.book_e_commerce.dto.category.CategoryRequestDTO;
 import br.com.buzzmonitor.book_e_commerce.dto.category.CategoryResponseDTO;
 import org.springframework.data.domain.Page;
@@ -8,9 +9,9 @@ import org.springframework.data.domain.Pageable;
 public interface CategoryService {
     void createCategory(CategoryRequestDTO categoryRequest);
 
-    Page<CategoryResponseDTO> getCategories(Pageable pageable);
+    Page<Category> getCategories(Pageable pageable);
 
-    CategoryResponseDTO getCategory(Long id);
+    Category getCategory(Long id);
 
     void updateCategory(Long id, CategoryRequestDTO categoryRequest);
 
--- src/main/java/br/com/buzzmonitor/book_e_commerce/service/impl/CategoryServiceImpl.java
+++ src/main/java/br/com/buzzmonitor/book_e_commerce/service/impl/CategoryServiceImpl.java
@@ -1,38 +1,57 @@
 package br.com.buzzmonitor.book_e_commerce.service.impl;
 
+import br.com.buzzmonitor.book_e_commerce.domain.Category;
 import br.com.buzzmonitor.book_e_commerce.dto.category.CategoryRequestDTO;
 import br.com.buzzmonitor.book_e_commerce.dto.category.CategoryResponseDTO;
+import br.com.buzzmonitor.book_e_commerce.mapper.CategoryMapper;
+import br.com.buzzmonitor.book_e_commerce.repository.CategoryRepository;
 import br.com.buzzmonitor.book_e_commerce.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
 
+    private final CategoryRepository categoryRepository;
 
     @Override
     public void createCategory(CategoryRequestDTO categoryRequest) {
+        Category categoryToSave = CategoryMapper.INSTANCE.toCategory(categoryRequest);
 
+        this.categoryRepository.save(categoryToSave);
     }
 
     @Override
-    public Page<CategoryResponseDTO> getCategories(Pageable pageable) {
-        return null;
+    public Page<Category> getCategories(Pageable pageable) {
+        return this.categoryRepository.findAll(pageable);
     }
 
     @Override
-    public CategoryResponseDTO getCategory(Long id) {
-        return null;
+    public Category getCategory(Long id) {
+        return this.categoryRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Category Not Found"));
     }
 
+    @Transactional
     @Override
     public void updateCategory(Long id, CategoryRequestDTO categoryRequest) {
+        Category categoryFound = this.getCategory(id);
 
+        Category categoryToUpdate = CategoryMapper.INSTANCE.toCategory(categoryRequest);
+
+        BeanUtils.copyProperties(categoryToUpdate, categoryFound);
+        this.categoryRepository.save(categoryFound);
     }
 
     @Override
     public void deleteCategory(Long id) {
-
+        this.categoryRepository.deleteById(id);
     }
 }
