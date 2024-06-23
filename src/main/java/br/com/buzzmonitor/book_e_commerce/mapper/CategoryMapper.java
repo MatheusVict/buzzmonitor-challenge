461480e74555011e8de376df38a14909027f8a54
Matheus Victor <matheusvictorhenrique@gmail.com>
2024-06-23 19:32:16 -0300

    feat: add mapStructer to project (#1)
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

---
+++ src/main/java/br/com/buzzmonitor/book_e_commerce/mapper/CategoryMapper.java
@@ -0,0 +1,17 @@
+package br.com.buzzmonitor.book_e_commerce.mapper;
+
+import br.com.buzzmonitor.book_e_commerce.domain.Category;
+import br.com.buzzmonitor.book_e_commerce.dto.category.CategoryRequestDTO;
+import br.com.buzzmonitor.book_e_commerce.dto.category.CategoryResponseDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface CategoryMapper {
+
+    CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
+
+    CategoryResponseDTO toCategoryResponseDTO(Category category);
+
+    Category toCategory(CategoryRequestDTO categoryRequestDTO);
+}
