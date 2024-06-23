e50168b7126f8d4e36b8429c857c60820dc48167
MatheusVict <matheusvictorhenrique@gmail.com>
2024-06-23 18:34:36 -0300

    feat: create category entity

---
+++ src/main/java/br/com/buzzmonitor/book_e_commerce/domain/Category.java
@@ -0,0 +1,32 @@
+package br.com.buzzmonitor.book_e_commerce.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
