03836e4439cce3ae28b99334eeed20a3509b39ba
MatheusVict <matheusvictorhenrique@gmail.com>
2024-06-24 16:37:03 -0300

    fix: fix search by name query

--- src/main/java/br/com/buzzmonitor/book_e_commerce/repository/BookRepository.java
+++ src/main/java/br/com/buzzmonitor/book_e_commerce/repository/BookRepository.java
@@ -2,12 +2,18 @@ package br.com.buzzmonitor.book_e_commerce.repository;
 
 import br.com.buzzmonitor.book_e_commerce.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     List<Book> findAllByCategoryName(String categoryName);
 
+    @Query("SELECT b FROM book b WHERE b.title LIKE %?1%")
     List<Book> findAllByTitle(String title);
+
+    Optional<Book> findBookByUuid(UUID uuid);
 }
