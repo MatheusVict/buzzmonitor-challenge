6af99a2e54c8ed7816ee183bbd5e75bb48e79cf6
MatheusVict <matheusvictorhenrique@gmail.com>
2024-06-24 22:10:04 -0300

    docs: add book controller swagger configurations
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

--- src/main/java/br/com/buzzmonitor/book_e_commerce/controller/BookController.java
+++ src/main/java/br/com/buzzmonitor/book_e_commerce/controller/BookController.java
@@ -4,9 +4,13 @@ import br.com.buzzmonitor.book_e_commerce.domain.Book;
 import br.com.buzzmonitor.book_e_commerce.dto.book.BookRequestDto;
 import br.com.buzzmonitor.book_e_commerce.dto.book.BookResponseDTO;
 import br.com.buzzmonitor.book_e_commerce.service.BookService;
+import br.com.buzzmonitor.book_e_commerce.swagger.annotations.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,43 +20,64 @@ import java.util.UUID;
 @RestController
 @RequestMapping("api/v1/books")
 @RequiredArgsConstructor
+@Tag(name = "Book", description = "Book API")
 public class BookController {
     private final BookService bookService;
 
     @GetMapping
+    @Operation(summary = "Get all books pageable", description = "Get all books pageable")
+    @OkResponse
     public ResponseEntity<Page<BookResponseDTO>> getAllPageable(Pageable pageable) {
         return ResponseEntity.ok(this.bookService.findAll(pageable));
     }
 
     @GetMapping("/category")
+    @Operation(summary = "Get all books by its category", description = "Get all books by its category")
+    @OkResponse
     public ResponseEntity<List<Book>> getBooksByCategory(@RequestParam("name") String categoryName) {
         return ResponseEntity.ok(this.bookService.getBooksByCategory(categoryName));
     }
 
 
     @GetMapping("/title")
+    @Operation(summary = "Get all books by its title", description = "Get all books by its title")
+    @OkResponse
     public ResponseEntity<List<BookResponseDTO>> getBooksByTitle(@RequestParam("title") String title) {
         return ResponseEntity.ok(this.bookService.getBooksByTitle(title));
     }
 
     @GetMapping("/{uuid}")
+    @Operation(summary = "Get a book by its uuid", description = "Get a book by its uuid")
+    @OkResponse
+    @NotFoundResponse
     public ResponseEntity<Book> getBookByUuid(@PathVariable UUID uuid) {
         return ResponseEntity.ok(this.bookService.getBookByUuid(uuid));
     }
 
     @PostMapping
+    @Operation(summary = "Create a book", description = "Create a book")
+    @CreatedResponse
+    @BadRequestResponse
     public ResponseEntity<Void> createBook(@RequestBody BookRequestDto bookRequestDto) {
         this.bookService.create(bookRequestDto);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PutMapping("/{uuid}")
+    @Operation(summary = "Update a book", description = "Update a book")
+    @NoContentResponse
+    @BadRequestResponse
+    @NotFoundResponse
     public ResponseEntity<Void> updateBook(@PathVariable UUID uuid, @RequestBody BookRequestDto bookRequestDto) {
         this.bookService.update(uuid, bookRequestDto);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{uuid}")
+    @Operation(summary = "Update a book", description = "Update a book")
+    @NoContentResponse
+    @BadRequestResponse
+    @NotFoundResponse
     public ResponseEntity<Void> deleteBook(@PathVariable UUID uuid) {
         this.bookService.delete(uuid);
         return ResponseEntity.ok().build();
--- src/main/java/br/com/buzzmonitor/book_e_commerce/dto/book/BookRequestDto.java
+++ src/main/java/br/com/buzzmonitor/book_e_commerce/dto/book/BookRequestDto.java
@@ -1,6 +1,7 @@
 package br.com.buzzmonitor.book_e_commerce.dto.book;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -11,17 +12,23 @@ import java.util.UUID;
 
 public record BookRequestDto(
         @NotBlank
+        @Schema(description = "Book title", example = "The Lord of the Rings")
         String title,
         @NotBlank
+        @Schema(description = "Book description", example = "The Lord of the Rings is an epic high-fantasy novel written by English author and scholar J. R. R. Tolkien.")
         String description,
         @NotBlank
+        @Schema(description = "Book author", example = "J. R. R. Tolkien")
         String author,
         @NotNull
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM/dd/yyyy")
+        @Schema(description = "Book publication date", example = "01/01/1954")
         Date publicationDate,
         @NotNull
+        @Schema(description = "Book quantity", example = "100")
         Integer stock,
         @NotNull
+        @Schema(description = "Category uuid", example = "123e4567-e89b-12d3-a456-426614174000")
         UUID category_uuid
 ) {
 
