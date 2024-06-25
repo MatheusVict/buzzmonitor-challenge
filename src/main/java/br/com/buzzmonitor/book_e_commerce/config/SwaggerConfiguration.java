f8af613f106e85287128362102cdb30a90a7c090
MatheusVict <matheusvictorhenrique@gmail.com>
2024-06-24 21:52:15 -0300

    chore: add swagger configs and swagger cors config
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

---
+++ src/main/java/br/com/buzzmonitor/book_e_commerce/config/SwaggerConfiguration.java
@@ -0,0 +1,38 @@
+package br.com.buzzmonitor.book_e_commerce.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SwaggerConfiguration {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Buzzmonitor API Challenge")
+                        .description("API for a book e-commerce")
+                        .version("1.0").contact(new Contact().name("Matheus Victor")
+                                .email("matheusvictorhenrique@gmail.com")
+                                .url("https://github.com/MatheusVict/3wings-full-stack-challenge"))
+                        .license(new License().name("License of API")
+                                .url("https://opensource.org/license/mit/")));
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
