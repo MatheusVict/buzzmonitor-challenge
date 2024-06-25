0c969b35bbf71bb8a900e7e356cdf0d1b25c92f4
MatheusVict <matheusvictorhenrique@gmail.com>
2024-06-24 22:20:31 -0300

    docs: add order controller swagger configurations
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

--- src/main/java/br/com/buzzmonitor/book_e_commerce/controller/OrderController.java
+++ src/main/java/br/com/buzzmonitor/book_e_commerce/controller/OrderController.java
@@ -3,6 +3,9 @@ package br.com.buzzmonitor.book_e_commerce.controller;
 import br.com.buzzmonitor.book_e_commerce.domain.Order;
 import br.com.buzzmonitor.book_e_commerce.dto.order.OrderRequestDTO;
 import br.com.buzzmonitor.book_e_commerce.service.OrderService;
+import br.com.buzzmonitor.book_e_commerce.swagger.annotations.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,31 +18,48 @@ import java.util.UUID;
 @RestController
 @RequestMapping("api/v1/order")
 @RequiredArgsConstructor
+@Tag(name = "Order", description = "Order API")
 public class OrderController {
     private final OrderService orderService;
 
     @GetMapping
+    @Operation(summary = "Get all orders", description = "Get all orders")
+    @OkResponse
     public ResponseEntity<Page<Order>> getOrders(Pageable pageable) {
         return ResponseEntity.ok(orderService.getAllOrders(pageable));
     }
 
     @GetMapping("/{uuid}")
+    @Operation(summary = "Get a order by its uuid", description = "Get a order by its uuid")
+    @OkResponse
+    @NotFoundResponse
     public ResponseEntity<Order> getOrderById(@PathVariable UUID uuid) {
         return ResponseEntity.ok(orderService.getOrderById(uuid));
     }
 
     @PostMapping
+    @Operation(summary = "Create a order", description = "Create a order")
+    @CreatedResponse
+    @BadRequestResponse
     public ResponseEntity<Order> createOrder(@RequestBody OrderRequestDTO order) {
         return ResponseEntity.status(HttpStatus.CREATED).body(orderService.createOrder(order));
     }
 
     @PutMapping("/{uuid}")
+    @Operation(summary = "update a order by its uuid", description = "Update a order by its uuid")
+    @NoContentResponse
+    @BadRequestResponse
+    @NotFoundResponse
     public ResponseEntity<Order> updateOrder(@PathVariable UUID uuid, @RequestBody OrderRequestDTO order) {
         orderService.updateOrder(uuid, order);
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/{uuid}")
+    @Operation(summary = "Create a order", description = "Create a order")
+    @NoContentResponse
+    @BadRequestResponse
+    @NotFoundResponse
     public ResponseEntity<Void> deleteOrder(@PathVariable UUID uuid) {
         orderService.deleteOrder(uuid);
         return ResponseEntity.noContent().build();
--- src/main/java/br/com/buzzmonitor/book_e_commerce/dto/order/OrderRequestDTO.java
+++ src/main/java/br/com/buzzmonitor/book_e_commerce/dto/order/OrderRequestDTO.java
@@ -1,9 +1,16 @@
 package br.com.buzzmonitor.book_e_commerce.dto.order;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+
 import java.util.UUID;
 
 public record OrderRequestDTO(
+        @NotBlank(message = "Book UUID is mandatory")
+        @Schema(description = "Book UUID", example = "123e4567-e89b-12d3-a456-426614174000")
         UUID bookId,
+        @NotBlank(message = "Amount is mandatory")
+        @Schema(description = "Amount", example = "1")
         Integer amount
 
 ) {
